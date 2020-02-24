f17659b67d78425bd01bb4a2af5105036dcfc07e
generalkuba <aaronngopwani@gmail.com>
2020-02-24 12:20:11 +0000

    commit test

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+/*const hello = document.getElementById('click');
+const button = document.getElementById('button');
+
+function eventhandler(ev){
+  console.log("Its been clicked");
+  ev.target.textContent = "Its been clicked"
+}
+
+button.addEventListener ('click', eventhandler);*/
